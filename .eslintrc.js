bb4a7f437fbf4f6fcdd9ca0ce99cc3d62e13ebaf
Mahendra Aryal <mahendra.aryal@cmd.com.au>
2023-10-11 07:33:55 +1100

    Added eslint, prettier, precommit hooks

---
+++ .eslintrc.js
@@ -0,0 +1,64 @@
+module.exports = {
+  settings: {
+    react: { version: 'detect' },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2021,
+    ecmaFeatures: {
+      jsx: true,
+    },
+    sourceType: 'module',
+  },
+  extends: ['eslint:recommended', 'prettier'],
+  rules: {
+    'no-console': 'error',
+  },
+  overrides: [
+    {
+      files: ['**/*.+(ts|tsx)'],
+      env: {
+        browser: true,
+      },
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: './tsconfig.eslint.json',
+      },
+      plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:prettier/recommended',
+      ],
+      rules: {
+        'react/jsx-uses-react': 'error',
+        'react/jsx-filename-extension': ['error', { extensions: ['.tsx'] }],
+        '@typescript-eslint/no-unused-vars': [
+          'error',
+          { argsIgnorePattern: '^_' },
+        ],
+        '@typescript-eslint/ban-ts-comment': 'off',
+        'react/display-name': 'off',
+        '@typescript-eslint/no-explicit-any': 'warn',
+        'react/prop-types': 'off',
+      },
+    },
+    {
+      files: ['**/__tests__/**'],
+      env: { jest: true },
+      plugins: ['jest', 'testing-library', 'prettier'],
+      extends: [
+        'plugin:jest/recommended',
+        // 'plugin:jest-dom/recommended',
+        'plugin:testing-library/react',
+      ],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'warn',
+      },
+    },
+  ],
+};
