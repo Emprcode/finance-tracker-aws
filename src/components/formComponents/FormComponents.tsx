8ad586e8b05435f9bdaf537d970bcfd4b16cbc0f
Narayan Aryal <aryal.coding@gmail.com>
2023-10-10 21:55:57 +1100

    basic layout created

---
+++ src/components/formComponents/FormComponents.tsx
@@ -0,0 +1,6 @@
+
+export const FormComponents = () => {
+  return (
+    <div>FormComponents</div>
+  )
+}
---
+++ src/pages/homepage/Homepage.tsx
@@ -0,0 +1,9 @@
+import { MainLayout } from "../../components/layout/mainLayout/MainLayout"
+
+const Homepage = () => {
+  return (
+    <MainLayout>Homepage</MainLayout>
+  )
+}
+
+export default Homepage
