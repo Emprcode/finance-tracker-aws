4ff7056a5b8d265920f375d01b17d37f5d1d1b39
Narayan Aryal <aryal.coding@gmail.com>
2023-10-14 21:10:44 +1100

    removed hardcoded config

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const congitoConfig = {
   REGION: 'ap-southeast-2',
-  USER_POOL_ID: 'ap-southeast-2_In0NFr1UQ',
-  USER_POOL_APP_CLIENT_ID: '3sh6cod004hg8ub5kakfg5hav',
+  USER_POOL_ID: process.env.REACT_APP_COGNITO_USER_POOL_ID,
+  USER_POOL_APP_CLIENT_ID: process.env.REACT_APP_COGNITO_USER_POOL_APP_CLIENT_ID,
 }
